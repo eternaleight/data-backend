8c3337f74b1a38e54592d91d2d3329f61b0dc0f1
eternaleight <eternaleight0@gmail.com>
2022-09-03 17:39:16 +0900

    feat(auth): add validation features

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const app = express()
-const dotenv = require("dotenv")
-dotenv.config()
-const cryptJS = require("crypt-js")
-const User = require("./src/v1/models/user")
-const jwt = require("jsonwebtoken")
 
+const app = express()
 app.use(express.json())
+app.use("/api/v1", require("./src/v1/routes/auth"))
 
 //DB接続
 try {
@@ -16,35 +12,6 @@ try {
 } catch (error) {
   console.log(error)
 }
-
-
-//ユーザー新規登録API
-app.post("/register", async(req, res) => {
-  //パスワードの受け取り
-  const password = req.body.password
-  try {
-    //パスワードの暗号化
-    req.body.password = cryptJS.AES.encrypt(password, process.env.SECRET_KEY)
-    //パスワードの新規作成
-    const user = await User.create(req.body)
-    //JWTの発行
-    const token = jwt.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
-      expiresIn: "24h",
-    })
-    return res.status(200).json({ user, token })
-  } catch (err) {
-    return res.status(500).json(err)
-  }
-})
-
-// app.put("/put"(req,res) => {
-// })
-
-// app.delete("/delete"(req,res) => {
-// })
-app.get("/", (res) => {
-  res.send("hello express")
-})
 
 const PORT = 5001
 app.listen(PORT, () => {
---
+++ src/v1/routes/auth.js
@@ -0,0 +1,53 @@
+const { body, validationResult } = require("express-validator")
+const cryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
+const router = require("express").Router()
+const User = require("../models/user")
+const dotenv = require("dotenv")
+dotenv.config()
+
+//ユーザー新規登録API
+router.post(
+  "/register",
+  body("username")
+    .isLength({ min: 8 })
+    .withMessage("ユーザー名は8文字以上である必要があります"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("パスワードは8文字以上である必要があります"),
+  body("confirmPassword")
+    .isLength({ min: 8 })
+    .withMessage("パスワードは8文字以上である必要があります"),
+  body("username").custom(async (value) => {
+    return User.findOne({ username: value }).then((user) => {
+      if (user) {
+        return Promise.reject("このユーザーは既に使われています")
+      }
+    })
+  }),
+  (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    next()
+  },
+  async (req, res) => {
+    //パスワードの受け取り
+    const password = req.body.password
+    try {
+      //パスワードの暗号化
+      req.body.password = cryptoJS.AES.encrypt(password, process.env.SECRET_KEY)
+      //パスワードの新規作成
+      const user = await User.create(req.body)
+      //JWTの発行
+      const token = jwt.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+        expiresIn: "24h",
+      })
+      return res.status(200).json({ user, token })
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  }
+)
+module.exports = router
